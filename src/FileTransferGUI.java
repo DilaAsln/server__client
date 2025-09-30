c318027b1068aa80a49d4e1ed39261f6965f85ea
dilasude <dilasudeaslan@gmail.com>
2025-09-30 23:21:11 +0300

    new add

--- src/FileTransferGUI.java
+++ src/FileTransferGUI.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 public class FileTransferGUI extends JFrame {
 
